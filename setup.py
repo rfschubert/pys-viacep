e08af0bffa9e84078e0a92eef19c4aab2c6ad9e4
Raphael Schubert <rfswdp@gmail.com>
2019-06-11 22:13:54 -0300

    updated setup.py with correct package name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='pys-viacep',
+    name='pysviacep',
     version='0.0.1',
     description='Consulta simplificada de enderecos pelo CEP no webservice VIACEP',
     url='https://github.com/rfschubert/pys-viacep',
     author='Raphael Schubert',
     author_email='rfswdp@gmail.com',
     license='MIT',
-    packages=['pys-viacep'],
+    packages=['pysviacep'],
     keywords=['python viacep', 'python3', 'viacep', 'schubert'],
     install_requires=[
         'requests'
